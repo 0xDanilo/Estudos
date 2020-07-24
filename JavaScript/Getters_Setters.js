cbc3748795dd61f2ca4c2d4f4e6dce850c1579f8
0xDanilo <danilo@riseup.net>
2020-07-23 21:50:14 -0300

    Getters
    Cria um objeto (ramanhoVariavel) com uma propriedade (tamanho) que é acessada diretamente através do chamada pelo metodo (get).

---
+++ JavaScript/Getters_Setters.js
@@ -0,0 +1,8 @@
+let tamanhoVariavel = {
+    get tamanho() {
+        return Math.floor(Math.random() * 100);
+    }
+};
+
+console.log(tamanhoVariavel.tamanho);
+console.log(tamanhoVariavel.tamanho);
