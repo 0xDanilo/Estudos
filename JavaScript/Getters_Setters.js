7d2aee442272bd9e773ae0284315620a5514e4ab
0xDanilo <danilo@riseup.net>
2020-07-24 22:07:56 -0300

    Getters  e Metodo
    Comparando getters com um método regular e os resultados de ambos.

--- JavaScript/Getters_Setters.js
+++ JavaScript/Getters_Setters.js
@@ -4,5 +4,16 @@ let tamanhoVariavel = {
     }
 };
 
+//Comparado com:
+
+let tamanhoVariavel_2 = {
+      gerarTamanho() {
+        return Math.floor(Math.random() * 100);
+    }
+};
+
 console.log(tamanhoVariavel.tamanho);
 console.log(tamanhoVariavel.tamanho);
+
+console.log(tamanhoVariavel_2.gerarTamanho());
+console.log(tamanhoVariavel_2.gerarTamanho());
