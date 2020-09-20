ca512386aa6839806fc7bc063712792f7b8fe353
0xDanilo <danilo@riseup.net>
2020-09-20 05:28:45 -0300

    Conjuntos
    Correções gramáticais.

--- C/conjuntos.c
+++ C/conjuntos.c
@@ -12,14 +12,14 @@ int main() {
     int x[] = {1,2,3,4,5,6};
     int y[] = {1,2,3,5,8,13};
 
-//Variáveis que seram utilizadas para o loop:
+//Variáveis utilizadas para o loop:
     int i, j;
 
     printf("R1 = "); 
     for(i = 0; i < 6; i++) {
         for (j = 0; j < 6; j++) {
             /*
-            Aqui fica condição para a combinação de conjuntos (x, y). 
+            Aqui fica condição para a combinação de conjuntos {x, y}. 
             Nesse exemplo a combinação foi "x + y > 10".
             */
             if (x[i] + y[j] > 10) {
