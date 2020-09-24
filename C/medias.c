428dff96894d451427817f12dccf3ec4e7ce538b
0xDanilo <danilo@riseup.net>
2020-09-23 21:28:14 -0300

    Medias
    Calcula as médias e arredonda para duas casas após a virgula.

--- C/medias.c
+++ C/medias.c
@@ -14,18 +14,14 @@ int main() {
     printf("Digite um valor para C: ");
     scanf("%i", &c);
 
-    //media aritimética:
+    //Médias:
     ma = (a+b+c)/3;
-    //Media harmonica, em C, precisa ser reescrita para somar 
-    //frações.
-    //mh = 3/(1/a+1/b+1/c);
     mg = cbrt((a*b*c));
     mp = (1*a+2*b+3*c)/(1+2+3);
 
-    printf("Media Aritimetica: %f \n", ma);
-    //printf("Media Harmonica: %f \n", mh);
-    printf("Media Geometrica: %f \n", mg);
-    printf("Media Ponderada: %f \n", mp);
+    printf("Media Aritimetica: %.2f \n", ma);
+    printf("Media Geometrica: %.2f \n", mg);
+    printf("Media Ponderada: %.2f \n", mp);
 
     return 0;
 }
