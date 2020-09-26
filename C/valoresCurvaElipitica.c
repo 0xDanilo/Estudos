e6297023e256ea5b095538b28b3360c55077d08a
0xDanilo <danilo@riseup.net>
2020-09-26 04:59:59 -0300

    Curva Eliptica
    Removida a parte que testava a curva com adição dos pontos P+Q pois estava errada.

--- C/valoresCurvaElipitica.c
+++ C/valoresCurvaElipitica.c
@@ -3,17 +3,18 @@
 
 int main() {
     
-    float x, y, pxQ;
+    float x, y, pxQ, somaPQ;
 
     printf("Valores para a Curva Eliptica na equação:\ny**2 = 4*(x**3) - 3*x + 2\n");
 
     for (x = -1; x < 1.0; x += 0.1)
     {
         y = 4*(pow(x, 3)) - 3*x + 2;
-        pxQ = x * y;
-
-        printf("{%.1f, %f} P*Q = {%f}\n", x, y, pxQ);
+        
+        printf("{%.1f, %f} \n", x, y);
     }
+
+
     
     return 0;
 }
