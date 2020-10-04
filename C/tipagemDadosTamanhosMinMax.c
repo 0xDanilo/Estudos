7c106d31612caf7d53da7155c553d36e739cf3be
0xDanilo <danilo@riseup.net>
2020-10-04 20:47:14 -0300

    Tipagem
    Alterado o para ordem crescente de tamanho e incluido o tipo "double".

--- C/tipagemDadosTamanhosMinMax.c
+++ C/tipagemDadosTamanhosMinMax.c
@@ -4,6 +4,10 @@
 
 int main() {
 
+	printf("O valor máximo de um short é: %i\n", SHRT_MAX);
+	printf("O valor minimo de um short é: %i\n", SHRT_MIN);
+	printf("Um short ocupa %zu bytes\n", sizeof(short));
+
 	printf("O valor máximo de um Inteiro é: %i\n", INT_MAX);
 	printf("O valor minimo de um Inteiro é: %i\n", INT_MIN);
 	printf("Um int ocupa %zu bytes\n", sizeof(int));
@@ -12,9 +16,10 @@ int main() {
 	printf("O valor minimo de um float é: %f\n", FLT_MIN);
 	printf("Um float ocupa %zu bytes\n", sizeof(float));
 
-	printf("O valor máximo de um short é: %i\n", SHRT_MAX);
-	printf("O valor minimo de um short é: %i\n", SHRT_MIN);
-	printf("Um short ocupa %zu bytes\n", sizeof(short));
+
+	printf("O valor máximo de um double é: %lf\n", DBL_MAX);
+	printf("O valor minimo de um double é: %lf\n", DBL_MIN);
+	printf("Um double ocupa %zu bytes\n", sizeof(double));
 
 	return 0;
 
