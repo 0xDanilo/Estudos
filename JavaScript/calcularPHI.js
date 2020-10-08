1db8079bdba6054db635514793037af73aa0ee94
0xDanilo <danilo@riseup.net>
2020-10-08 06:20:03 -0300

    Classes - Correlação
    Mudança na forma de passar os parametros para a função phi(), dessa vez passado os valores como propriedades do objeto (teste, teste2).
    Ficou em texto comentado a maneira anterior que estava usando.

--- JavaScript/calcularPHI.js
+++ JavaScript/calcularPHI.js
@@ -1,8 +1,11 @@
 class Correlacao{
+//"Constructor" cria as poproedades que um objeto dessa classe podera receber
     constructor(nomeCorrelacao, resultado) {
         this.nomeCorrelacao = nomeCorrelacao;
         this.resultado = resultado;
     }
+
+//Metódos
     phi([n00, n01, n10, n11]) {
         return (n00 * n11 - n10 * n01) / 
             Math.sqrt((n10 + n00) *
@@ -10,6 +13,7 @@ class Correlacao{
                     (n01 + n00) *
                     (n11 + n10));
     }
+
     resultadoPhi(resultado) {
         if (resultado > 1) {
             console.log(`A correlação entre ${this.nomeCorrelacao} apresentou um resultado acima de 1. Verfique os dados.`);
@@ -24,8 +28,15 @@ class Correlacao{
     }
 }
 
-let teste = new Correlacao("pizza e esquilo");
-teste.resultadoPhi(teste.phi([76, 9, 4, 100]));
+let teste = new Correlacao("Evento 1 e Evento 2", [76, 9, 4, 100]);
+teste.resultadoPhi(teste.phi(teste.resultado));
 
-let teste2 = new Correlacao("Trabalho e riqueza");
-teste2.resultadoPhi(teste2.phi([8000, 100000, 4, 60]));
+let teste2 = new Correlacao("Evento 3 e Evento 4", [8000, 100000, 4, 60]);
+teste2.resultadoPhi(teste2.phi(teste2.resultado));
+
+//teste2.resultadoPhi(teste2.phi([8000, 100000, 4, 60]));
+
+//imprime na tela os objetos e suas propriedades:
+console.log("\n");
+console.log(teste);
+console.log(teste2);
