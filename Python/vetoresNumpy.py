e4f75600669232069c7fe93dee4a240da638aaa6
0xDanilo <danilo@riseup.net>
2020-10-10 20:56:40 -0300

    Comentários
    Comentários.

--- Python/vetoresNumpy.py
+++ Python/vetoresNumpy.py
@@ -17,8 +17,9 @@ print(f'\nRetorne "True" para as posições ainda o elemento "2" for encontrado:
 # e retonar "True" para elas. 
 encontraNumero3 = elementos == 3
 print(f'\nRetorne "True" para as posições ainda o elemento "3" for encontrado: \n{encontraNumero3}')
-# Realiza a operação Booleana
-print(f'\nOperação lógica "and" entre os dois vetores resultantes: \n {np.logical_and(encontraNumero2, encontraNumero3)}')
+
+# Realiza a operação Lógica AND
+print(f'\nOperação lógica "AND" entre os dois vetores resultantes: \n {np.logical_and(encontraNumero2, encontraNumero3)}')
 
 print(f'\nA matriz possui {elementos.ndim} dimensões e {elementos.shape} posições.')
 
@@ -26,6 +27,7 @@ print(f'\nA matriz possui {elementos.ndim} dimensões e {elementos.shape} posiç
 print(f'Elementos da segunda linha: {elementos[1:2]}')
 print(f'Elementos da segunda coluna: {elementos[:, 1]}')
 
+#Calcula a Média, Desvio Padrão, Variância e arredonda os valores para 2 casas depois da vírgula
 print(f'\nMédia: {round(np.average(elementos), 2)}')
 print(f'Variância: {round(np.var(elementos), 2)}')
 print(f'Desvio Padrão: {round(np.std(elementos), 2)}')
