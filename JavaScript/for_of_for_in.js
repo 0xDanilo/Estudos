edada67afe25bf12f27ad0582395ac5470573078
0xDanilo <danilo@riseup.net>
2020-11-13 01:39:46 -0300

    Loops - For/of

---
+++ JavaScript/for_of_for_in.js
@@ -0,0 +1,21 @@
+const vetor = [2, 3, 5, 7, 11]; // objeto iterável
+let soma = 0;
+
+// Para o loop de for/of o objeto deve ser iterável
+for (const numeros of vetor) {
+    soma += numeros;
+}
+console.log(soma);
+
+// Para objetos não iterável, é possivel utilizar os métodos:
+// Object.keys(nome_do_objeto);
+// Object.values(nome_do_objeto);
+// Object.entries(nome_do_objeto);
+
+const objeto_nao_iteravel = {a: 2, b: 3, c: 5, d: 7, e: 11};
+let soma2 = 0;
+for (const valores of Object.values(objeto_nao_iteravel)) {
+    soma2 += valores;
+}
+console.log(soma2);
+
