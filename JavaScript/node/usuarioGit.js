d67d364e3246fe61a69d584f2a3a51457b2fdb3f
0xDanilo <danilo@riseup.net>
2020-11-29 14:07:54 -0300

    Començando Async
    Cria uma função assíncrona que puxa dados do perfil do usuário via a API do Github.

---
+++ JavaScript/node/usuarioGit.js
@@ -0,0 +1,29 @@
+// Começando async/await
+// Exemplo derivado: https://www.taniarascia.com/promise-all-with-async-await/
+
+// Dependências
+const FETCH = require("node-fetch");
+// Entrada de dados:
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Função Async, a qual exibe dados do usuário utilizando a API do Github:
+async function infoUsuario(usuario) {
+    try {
+      const RESPOSTA = await FETCH('https://api.github.com/users/'+usuario);
+      const INFO = await RESPOSTA.json();
+      console.log(INFO);
+
+    } catch (erro) {
+// Em caso de erro:
+      console.error(erro);
+    }
+  }
+  
+// Entrada de Dados do usuário:
+  readline.question('Digite o nome do usuário: ', name => {
+    infoUsuario(name);
+    readline.close();
+  });
